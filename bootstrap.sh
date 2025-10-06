75474982d28dee634b681e7c92b137759a49a6d2
dozvald <thatisjustagame@gmail.com>
2025-10-06 23:19:04 +0200

    Fix docker policy non-root user as default

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -eu
 
+SOURCE_DIR="/source"
+TARGET_DIR="/target"
+
 if [ ! -d /target ]; then
   echo "Error - No volume mounted on /target !"
   echo "Please use one of the following commands:"
@@ -10,8 +13,39 @@ if [ ! -d /target ]; then
   exit 1
 fi
 
+# Gets user id and group id
+UID_CUR=$(id -u)
+GID_CUR=$(id -g)
+
+echo "Running as UID=$UID_CUR GID=$GID_CUR"
+
+if [ ! -d "$TARGET_DIR" ]; then
+  echo "Error - Target directory $TARGET_DIR does not exist or is not a directory."
+  exit 1
+fi
+
+# Check current user rights
+if [ ! -w "$TARGET_DIR" ]; then
+  echo "Error - $TARGET_DIR not writable by UID=$UID_CUR."
+  
+  # if user is not root, try with root user
+  if [ "$UID_CUR" -ne 0 ]; then
+    echo "Retrying as root..."
+	#replaces the process with a new user (root)
+    exec su -s /bin/sh root -c "$0 $@"
+  else
+    echo "Error - Even root cannot write to $TARGET_DIR. Check volume permissions."
+    exit 1
+  fi
+fi
+
 echo "Starting copy..."
-mkdir /target/vps-saas-deployer
-cp -rnv /source/* /target/vps-saas-deployer
-rm /target/vps-saas-deployer/bootstrap.sh
-echo "Package correctly deployed. Take a quick moment to configure variables before using. Enjoy."
+mkdir $TARGET_DIR/vps-saas-deployer
+cp -rnv $SOURCE_DIR/* $TARGET_DIR/vps-saas-deployer
+rm $TARGET_DIR/vps-saas-deployer/bootstrap.sh
+
+FINAL_UID=$(id -u)
+FINAL_USER=$(id -un 2>/dev/null || echo "UID:$FINAL_UID")
+
+echo "Success - Files were copied by user '$FINAL_USER' (UID=$FINAL_UID) in folder: $TARGET_DIR."
+echo "Package correctly deployed. Please take a look at Readme file. Enjoy."
--- dockerfile
+++ dockerfile
@@ -1,9 +1,14 @@
-FROM alpine:latest
+FROM busybox:1.37.0
+
+LABEL maintainer="david.ozvald@gmail.com"
 
 WORKDIR /source
 COPY . .
+
+# Creates non-root user
+RUN addgroup -S appgroup && adduser -S appuser -G appgroup
 RUN chmod +x bootstrap.sh
 
-LABEL maintainer="david.ozvald@gmail.com"
+USER appuser
 
 CMD ["./bootstrap.sh"]
